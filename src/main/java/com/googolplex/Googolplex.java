d8c304410b949ad4371d4275cd1bd6f8c74774e2
Anthaguz <anthonyag7@hotmail.com>
2023-02-19 21:23:11 -0600

    Final2

---
+++ src/main/java/com/googolplex/Googolplex.java
@@ -0,0 +1,13 @@
+package com.googolplex;
+
+import javax.swing.JFileChooser;
+
+public class Googolplex {
+
+    //Variables "globales" que usan el contenido guardado localmente.
+    
+    public static void main(String[] args) {
+        //Program start
+    }
+     
+}
