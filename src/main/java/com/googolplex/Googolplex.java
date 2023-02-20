afd373cef5241c091486eed7722581156ff64950
Anthaguz <anthonyag7@hotmail.com>
2023-02-19 22:06:46 -0600

    Initial commit, initializing project

--- src/main/java/com/googolplex/Googolplex.java
+++ src/main/java/com/googolplex/Googolplex.java
@@ -1,13 +1,12 @@
 package com.googolplex;
 
-import javax.swing.JFileChooser;
-
 public class Googolplex {
 
     //Variables "globales" que usan el contenido guardado localmente.
     
     public static void main(String[] args) {
         //Program start
+        
     }
      
 }
